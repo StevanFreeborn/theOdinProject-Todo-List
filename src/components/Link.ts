4326fcf07a363520cff15d7c51bdfdb6759b51a0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-08-02 10:21:53 -0500

    feat: add add list link to sidebar

--- src/components/Link.ts
+++ src/components/Link.ts
@@ -1,12 +1,30 @@
 type LinkProps = {
   linkText: string;
   href: string;
-  styles: string;
+  styles?: string;
+  onMouseOver?: (event: Event, target: HTMLElement) => void;
+  onMouseOut?: (event: Event, target: HTMLElement) => void;
 };
 
-export default function Link({ linkText, href, styles }: LinkProps) {
+export default function Link({
+  linkText,
+  href,
+  styles,
+  onMouseOver,
+  onMouseOut,
+}: LinkProps) {
+  // if href is an absolute one append
+  // the envs base path.
+  const hrefText = /^\/$/.test(href) ? process.env.BASE_PATH + href : href;
+
   return /*html*/ `
-    <a href="${process.env.BASE_PATH + href}" style="${styles}" data-link>
+    <a 
+      href="${hrefText}" 
+      style="${styles}"
+      onmouseover="(${onMouseOver})(event, this)"
+      onmouseout="(${onMouseOut})(event, this)"
+      data-link
+    >
       ${linkText}
     </a>
   `;
--- src/components/Sidebar.ts
+++ src/components/Sidebar.ts
@@ -43,10 +43,21 @@ export default function SideBar() {
     padding: '1rem 0rem',
   });
 
-  const navListTitleStyles = inlineStyles({
+  const navListTitleContainer = inlineStyles({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     padding: '0rem 1rem',
-    fontSize: '1.15rem',
     color: '#ffffff',
+  });
+
+  const navListTitleStyles = inlineStyles({
+    fontSize: '1.15rem',
+    fontWeight: 'bold',
+  });
+
+  const addListLinkStyles = inlineStyles({
+    fontSize: '1.25rem',
     fontWeight: 'bold',
   });
 
@@ -74,7 +85,16 @@ export default function SideBar() {
             .join('')}
         </ul>
         <div>
-          <div style="${navListTitleStyles}">My lists</div>
+          <div style="${navListTitleContainer}">
+            <div style="${navListTitleStyles}">My lists</div>
+            ${Link({
+              linkText: '+',
+              href: '/lists/add',
+              styles: addListLinkStyles,
+              onMouseOver: (e, t) => (t.style.color = '#424242'),
+              onMouseOut: (e, t) => (t.style.color = ''),
+            })}
+          </div>
           <ul style="${navListStyles}">
           ${secondaryNavLinkItems
             .map(item =>
